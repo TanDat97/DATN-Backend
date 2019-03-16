ebb77ed54d1a78d085492e1ea0de516b6b5e266c
Dat dep trai <trantandat130497@gmail.com>
2019-03-16 11:18:14 +0700

    login

--- myapp/app.js
+++ myapp/app.js
@@ -1,14 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var app = express();
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
+const createError = require('http-errors');
+const express = require('express');
+const app = express();
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var projectsRouter = require('./routes/projects');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const projectsRouter = require('./routes/projects');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //     useNewUrlParser: true
 // });
 mongoose.connect('mongodb+srv://dat:datdeptrai123@cluster0-mmyqj.mongodb.net/realestate?retryWrites=true',{
-    useNewUrlParser: true
+  useNewUrlParser: true
 });
 mongoose.Promise = global.Promise;
 
@@ -41,7 +42,6 @@ app.use(function(err, req, res, next) { // error handler
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');

b2322b00661fdcec784afd324a52d85ccc87e208
Dat dep trai <trantandat130497@gmail.com>
2019-07-09 15:16:22 +0700

    change mongo db

--- config/mongo.js
+++ config/mongo.js
@@ -1,12 +1,12 @@
-const  url = 'mongodb+srv://@cluster0-mmyqj.mongodb.net/realestate?retryWrites=true'
-const options = {
-    user: "tuan",
-    pass: "tuan123",
-    auth: {
-        authdb: 'admin'
-    },
-    useNewUrlParser: true,
-}
+// const  url = 'mongodb+srv://@cluster0-mmyqj.mongodb.net/realestate?retryWrites=true'
+// const options = {
+//     user: "tuan",
+//     pass: "tuan123",
+//     auth: {
+//         authdb: 'admin'
+//     },
+//     useNewUrlParser: true,
+// }
 
 // const url = 'mongodb://127.0.0.1:27017/realestate?retryWrites=true'
 // const options = {
@@ -17,6 +17,16 @@ const options = {
 //     // },
 //     useNewUrlParser: true,
 // }
+
+const url = 'mongodb://127.0.0.1:27017/realestate?retryWrites=true'
+const options = {
+    user: "admin",
+    pass: "admin123",
+    auth: {
+        authdb: 'admin'
+    },
+    useNewUrlParser: true,
+}
 
 module.exports.url = url
 module.exports.options = options

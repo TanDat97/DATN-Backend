7a02227619b16bf2d1ac7fd6cc16c5e7c9f15255
Dat dep trai <trantandat130497@gmail.com>
2019-07-09 21:09:48 +0700

    change api for google login

--- middleware/config.js
+++ middleware/config.js
@@ -1,7 +1,9 @@
+const constant = require('../lib/constant')
+
 module.exports = {
     'googleAuth' : {
         'clientID'         : '398072486886-538mgmvf0dvn7vb8m8cm9jq039d4k2es.apps.googleusercontent.com',
         'clientSecret'     : '8dyRJPDzRg3oq7aRGpq6cW0-',
-        'callbackURL'      : 'http://localhost:3001//users/auth/google/callback'
+        'callbackURL'      :  constant.hostApi + '/users/auth/google/callback'
     }
 }
